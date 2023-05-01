93f0e2f801c644ddb6117dda13a25846748ad35d
aleksandar <sleasarbajic@gmail.com>
2023-05-01 23:45:26 +0200

    sasa

--- pages/[watch]/index.js
+++ pages/[watch]/index.js
@@ -10,17 +10,21 @@ export default function Player() {
       autoplay
       src={[
         {
-          src: "https://creckflix.s3.eu-north-1.amazonaws.com/Operation.Fortune.Ruse.de.Guerre.2023.720p.WEBRip.800MB.x264-GalaxyRG.ogv",
-          type: "video/ogg",
+          src: "https://creckflix.s3.eu-north-1.amazonaws.com/Top.Gun.Maverick.2022.IMAX.720p.WEBRip.900MB.x264-GalaxyRG.mkv",
+          type: "video/mkv",
         },
-        {
-          src: "https://creckflix.s3.eu-north-1.amazonaws.com/Operation.Fortune.Ruse.de.Guerre.2023.720p.WEBRip.800MB.x264-GalaxyRG.avi",
-          type: "video/avi",
-        },
-        {
-          src: "https://creckflix.s3.eu-north-1.amazonaws.com/Operation.Fortune.Ruse.de.Guerre.mp4",
-          type: "video/mp4",
-        },
+        // {
+        //   src: "https://creckflix.s3.eu-north-1.amazonaws.com/Operation.Fortune.Ruse.de.Guerre.2023.720p.WEBRip.800MB.x264-GalaxyRG.ogv",
+        //   type: "video/ogg",
+        // },
+        // {
+        //   src: "https://creckflix.s3.eu-north-1.amazonaws.com/Operation.Fortune.Ruse.de.Guerre.2023.720p.WEBRip.800MB.x264-GalaxyRG.avi",
+        //   type: "video/avi",
+        // },
+        // {
+        //   src: "https://creckflix.s3.eu-north-1.amazonaws.com/Operation.Fortune.Ruse.de.Guerre.mp4",
+        //   type: "video/mp4",
+        // },
       ]}
       poster="https://media-files.vidstack.io/poster.png"
       controls
