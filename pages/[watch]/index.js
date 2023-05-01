a53f9281334cec77e493061c438de457a73320fa
aleksandar <sleasarbajic@gmail.com>
2023-05-01 21:44:38 +0200

    sd

--- pages/[watch]/index.js
+++ pages/[watch]/index.js
@@ -7,9 +7,10 @@ export default function Player() {
   return (
     <MediaPlayer
       src={[
-        { src: "https://media-files.vidstack.io/720p.ogv", type: "video/ogg" },
-        { src: "https://media-files.vidstack.io/720p.avi", type: "video/avi" },
-        { src: "https://media-files.vidstack.io/720p.mp4", type: "video/mp4" },
+        {
+          src: "https://creckflix.s3.eu-north-1.amazonaws.com/Operation.Fortune.Ruse.de.Guerre.mp4",
+          type: "video/mp4",
+        },
       ]}
       poster="https://media-files.vidstack.io/poster.png"
       controls
