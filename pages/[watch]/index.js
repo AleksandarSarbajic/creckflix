fe92526011105454c8b768203ac758db6ed9822e
aleksandar <sleasarbajic@gmail.com>
2023-05-01 22:05:10 +0200

    dsds

--- pages/[watch]/index.js
+++ pages/[watch]/index.js
@@ -7,7 +7,10 @@ export default function Player() {
   return (
     <MediaPlayer
       src={[
-        { src: "https://media-files.vidstack.io/720p.mp4", type: "video/mp4" },
+        {
+          src: "https://creckflix.s3.eu-north-1.amazonaws.com/3X4+ANUBIS.mp4",
+          type: "video/mp4",
+        },
       ]}
       controls
     >
