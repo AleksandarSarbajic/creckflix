5e8b705b7f9696c34caa086e8376e9cdfe34d78a
aleksandar <sleasarbajic@gmail.com>
2023-05-01 21:39:48 +0200

    movie

--- pages/[watch]/index.js
+++ pages/[watch]/index.js
@@ -3,16 +3,17 @@ import "vidstack/styles/defaults.css";
 
 import { MediaOutlet, MediaPlayer } from "@vidstack/react";
 
-export default function Watch() {
-  const router = useRouter();
-  console.log(router);
+export default function Player() {
   return (
     <MediaPlayer
-      src="https://creckflix.s3.eu-north-1.amazonaws.com/Operation.Fortune.Ruse.de.Guerre.mp4"
+      src={[
+        { src: "https://media-files.vidstack.io/720p.ogv", type: "video/ogg" },
+        { src: "https://media-files.vidstack.io/720p.avi", type: "video/avi" },
+        { src: "https://media-files.vidstack.io/720p.mp4", type: "video/mp4" },
+      ]}
       poster="https://media-files.vidstack.io/poster.png"
       controls
     >
-      {/* ^ remove `controls` attribute if you're designing a custom UI */}
       <MediaOutlet />
     </MediaPlayer>
   );
