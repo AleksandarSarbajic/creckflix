209b298c68a76d5410e1dfb721e65ae225f79f52
aleksandar <sleasarbajic@gmail.com>
2023-05-01 23:54:11 +0200

    sasa

--- pages/[watch]/index.js
+++ pages/[watch]/index.js
@@ -7,7 +7,6 @@ export default function Player() {
   return (
     <MediaPlayer
       load={"eager"}
-      autoplay
       src={[
         {
           src: "https://creckflix.s3.eu-north-1.amazonaws.com/Operation.Fortune.Ruse.de.Guerre.2023.720p.WEBRip.800MB.x264-GalaxyRG.ogv",
@@ -22,7 +21,6 @@ export default function Player() {
           type: "video/mp4",
         },
       ]}
-      poster="https://media-files.vidstack.io/poster.png"
       controls
     >
       {/* dsds */}
